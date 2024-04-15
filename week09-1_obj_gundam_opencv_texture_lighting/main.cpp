cef261c650569eb1f732b18d5c2ca16bcdf6cd91
蔡耀斌 <112918606+tsaiyaobin@users.noreply.github.com>
2024-04-15 16:39:50 +0800

    week09

--- week09-1_obj_gundam_opencv_texture_lighting/main.cpp
+++ week09-1_obj_gundam_opencv_texture_lighting/main.cpp
@@ -33,14 +33,27 @@ void drawmodel(void)
         glmVertexNormals(pmodel, 90.0);
     }
 
-    glmDraw(pmodel, GLM_SMOOTH | GLM_MATERIAL | GLM_TEXTURE);
+    glmDraw(pmodel, GLM_SMOOTH | GLM_TEXTURE);
 }
+float angle=0;
 void display()
 {
     glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT);
-    drawmodel(); ///glutSolidTeapot( 0.3 );
+    glPushMatrix();
+        glRotatef(angle++,0,1,0);
+        drawmodel(); ///glutSolidTeapot( 0.3 );
+    glPopMatrix();
     glutSwapBuffers();
 }
+const GLfloat light_ambient[]  = { 0.0f, 0.0f, 0.0f, 1.0f };
+const GLfloat light_diffuse[]  = { 1.0f, 1.0f, 1.0f, 1.0f };
+const GLfloat light_specular[] = { 1.0f, 1.0f, 1.0f, 1.0f };
+const GLfloat light_position[] = { 2.0f, 5.0f, -5.0f, 0.0f };
+
+const GLfloat mat_ambient[]    = { 0.7f, 0.7f, 0.7f, 1.0f };
+const GLfloat mat_diffuse[]    = { 0.8f, 0.8f, 0.8f, 1.0f };
+const GLfloat mat_specular[]   = { 1.0f, 1.0f, 1.0f, 1.0f };
+const GLfloat high_shininess[] = { 100.0f };
 int main(int argc, char* argv[])
 {
     glutInit(&argc, argv);
@@ -48,6 +61,23 @@ int main(int argc, char* argv[])
     glutCreateWindow("week07-2 obj gundam opencv texture");
     glutDisplayFunc(display);
     myTexture("Diffuse.jpg");
+        glEnable(GL_DEPTH_TEST);
+        glDepthFunc(GL_LESS);
 
+        glEnable(GL_LIGHT0);
+        glEnable(GL_NORMALIZE);
+        glEnable(GL_COLOR_MATERIAL);
+        glEnable(GL_LIGHTING);
+
+        glLightfv(GL_LIGHT0, GL_AMBIENT,  light_ambient);
+        glLightfv(GL_LIGHT0, GL_DIFFUSE,  light_diffuse);
+        glLightfv(GL_LIGHT0, GL_SPECULAR, light_specular);
+        glLightfv(GL_LIGHT0, GL_POSITION, light_position);
+
+        glMaterialfv(GL_FRONT, GL_AMBIENT,   mat_ambient);
+        glMaterialfv(GL_FRONT, GL_DIFFUSE,   mat_diffuse);
+        glMaterialfv(GL_FRONT, GL_SPECULAR,  mat_specular);
+        glMaterialfv(GL_FRONT, GL_SHININESS, high_shininess);
+    glutIdleFunc(display);
     glutMainLoop();
 }
